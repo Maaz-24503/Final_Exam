189b9bc0bf495acc3cdc90d9e2f1c2b019f73934
Maaz-24503 <maazkarim84@gmail.com>
2024-05-26 21:16:39 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# appdev_final_exam
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:appdev_final_exam/firebase_options.dart';
+import 'package:appdev_final_exam/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ProviderScope(child: MainApp()));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
